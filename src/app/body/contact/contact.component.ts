d481cc1309aa9374fdc6557b0af5de914dd4da06
RonStrauss <ashbelthor@gmail.com>
2023-06-21 10:05:33 +0300

    regex

--- src/app/body/contact/contact.component.ts
+++ src/app/body/contact/contact.component.ts
@@ -9,10 +9,12 @@ import { FormBuilder, Validators, FormGroup, FormArray } from '@angular/forms';
 export class ContactComponent {
   constructor(private _fb: FormBuilder) {}
 
+  alphabeticRegex = () => new RegExp(/^[\D\w]+$/  , 'u')
+
   contactForm = this._fb.nonNullable.group({
     email: ['', [Validators.email, Validators.required]],
-    firstName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+$/)]],
-    lastName: ['', [Validators.required, Validators.pattern(/^[a-zA-Z]+$/)]],
+    firstName: ['', [Validators.required, Validators.pattern(this.alphabeticRegex())]],
+    lastName: ['', [Validators.required, Validators.pattern(this.alphabeticRegex())]],
     phone: ['', [Validators.required, Validators.pattern(/\d/)]],
   });
 }
