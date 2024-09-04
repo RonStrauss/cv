96bfd089564bfe8d1abdc31dc3296e53192f910e
RonStrauss <ashbelthor@gmail.com>
2024-09-04 09:29:01 +0300

    update email

--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,8 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Ron Strauss';
-export const SITE_DESCRIPTION = 'My personal website and blog. I write about web development, programming, and other tech topics. Here you can also find my portfolio and contact information. Have a look around!';
+export const SITE_DESCRIPTION = 'My personal website. Here you can also find my portfolio and contact information. Have a look around!';
+
+export const DOMAIN = 'ronstrauss.cc';
+export const SITE_URL = `https://${DOMAIN}`;
+export const MY_EMAIL = `hi@${DOMAIN}`;
